2172c08932f2a0d987e10a9fcafe611317cb0a3a
Xavo <alanxavi777@gmail.com>
2026-07-04 12:39:33 -0500

    Api 1.0

---
+++ src/test/java/com/condominio/condominio_api/CondominioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condominio.condominio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondominioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
